fdc8acd28ff9d8c9190aa6eef68010c3c56df2c4
Giulio Garofalo <giuliogarofalo91@gmail.com>
2023-03-30 10:53:35 +0200

    ok mo cahce

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
-import { ref, computed, onMounted, watch} from "vue";
+import { ref, computed} from "vue";
 import LocationTools from "./components/LocationTools.vue";
 import CurrentWeather from "./components/CurrentWeather.vue";
 
 const isCelsius = ref(true)
 const weather = ref(undefined)
 const stateNavigator = ref(0)
+const urlWeather = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
 
-// const name = computed(() => `${weather.value.name}, ${weather.value.sys.country ?? ''}`)
-// const sunrise =  computed(() => new Date(weather.value.sys.sunrise * 1000).getHours())
-// const sunset = computed(() => new Date(weather.value.sys.sunset * 1000).getHours());
+const name = computed(() => `${weather.value.name}, ${weather.value.sys.country ?? ''}`)
+const sunrise =  computed(() => new Date(weather.value.sys.sunrise * 1000).getHours())
+const sunset = computed(() => new Date(weather.value.sys.sunset * 1000).getHours());
 
 const switchMeasurement = () => {
   isCelsius.value = !isCelsius.value
 }
 
-const urlWeather = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
 
-const fetchWithRetry = async (url, opts, tries=10) => {
+const fetchWithRetry = async (url, opts, tries=5) => {
   const errs = [];
   for (let i = 0; i < tries; i++) {
     console.log(`trying GET '${url}' [${i + 1} of ${tries}]`);
@@ -36,7 +36,6 @@ fetchWithRetry(urlWeather)
   .then(data => weather.value = data)
   .catch(err => console.error(err));
 
-
 async function weatherLocation(location) {
   const urlWeather = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
   const response = await fetch(urlWeather);
@@ -52,64 +51,6 @@ async function weatherLocation(location) {
 weatherLocation().catch(error => {
   console.log(error.message); // 'An error has occurred: 404'
 });
-
-// const weatherLocation = async (location) => {
-  
-  
-  
-  
-//   fetch(urlWeather)
-//   .then(response => 
-//     // if(response.status === 404) {
-//     //   throw 'City no found'
-//     // }
-//       response.json()
-    
-//   )
-//   .then(data => {
-//     console.log(data)
-//     weather.value = await data
-//   }).catch(error => console.error(error))
-  
-  
-// //   fetch(urlWeather)
-// //   .then((response) => {
-// //     if (response.status === 404) {
-// //       throw 'City no found'
-// //     }
-// //     if(response.ok) {
-// //       return response
-// //     }
-// //   // console.log('Looks like there was a problem. Status Code: ' + response.status);
-// //         // weather.value = response.data;
-
-// // //  return response.data
-  
-// //  }).then(res => console.log(res.json()) )
-
-//   // const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
-//   //   try {
-    
-//   //     const resolve = await Promise.all([
-//   //       fetch(urlWeather),
-//   //       fetch(urlForecast)
-//   //     ])
-//   //     if (resolve[0].status == 404 || resolve[1].status == 404) {
-//   //       throw 'City no found'
-//   //     }
-//   //     const data = resolve.map(response => response.json());
-//   //     weather.value = await data[0];
-      
-//   //     //filter the list index 40/8
-//   //     const forecastOrigin = await data[1]
-//   //     const forecastDays = filterByIncrement(forecastOrigin.list,  8, 40)
-
-//   //     forecast.value = forecastDays
-//   //   } catch(err) {
-//   //     console.error(err);
-//   //   }
-
-// }
 
 function locationBySearch(query) {
   if (query != ''){
@@ -146,7 +87,7 @@ async function getLocation() {
       <div class="flex flex-col sm:flex-row gap-4" v-if="weather">
        <div class="w-full sm:w-6/12 md:w-6/12 grow">
           <CurrentWeather
-            :name="weather.name"
+            :name="name"
             :temp="weather.main.temp"
             :feels_like="weather.main.feels_like"
             :main=" weather.weather[0].main"
