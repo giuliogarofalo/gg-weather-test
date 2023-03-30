8e526a100e784d752f46a61f3bd427681428fed5
Giulio Garofalo <giuliogarofalo91@gmail.com>
2023-03-30 10:52:22 +0200

    non si è sfondato

--- src/App.vue
+++ src/App.vue
@@ -4,54 +4,112 @@ import LocationTools from "./components/LocationTools.vue";
 import CurrentWeather from "./components/CurrentWeather.vue";
 
 const isCelsius = ref(true)
-const weather = ref({})
-const forecast = ref([])
+const weather = ref(undefined)
 const stateNavigator = ref(0)
 
-const name = computed(() => `${weather.value.name}, ${weather.value.sys.country ?? ''}`)
-const sunrise =  computed(() => new Date(weather.value.sys.sunrise * 1000).getHours())
-const sunset = computed(() => new Date(weather.value.sys.sunset * 1000).getHours());
+// const name = computed(() => `${weather.value.name}, ${weather.value.sys.country ?? ''}`)
+// const sunrise =  computed(() => new Date(weather.value.sys.sunrise * 1000).getHours())
+// const sunset = computed(() => new Date(weather.value.sys.sunset * 1000).getHours());
 
 const switchMeasurement = () => {
   isCelsius.value = !isCelsius.value
 }
 
-const weatherLocation = async (location) => {
+const urlWeather = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
 
-  const urlWeather = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
-  const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
+const fetchWithRetry = async (url, opts, tries=10) => {
+  const errs = [];
+  for (let i = 0; i < tries; i++) {
+    console.log(`trying GET '${url}' [${i + 1} of ${tries}]`);
     try {
-    
-      const resolve = await Promise.all([
-        fetch(urlWeather),
-        fetch(urlForecast)
-      ])
-      if (resolve[0].status == 404 || resolve[1].status == 404) {
-        throw 'City no found'
-      }
-      const data = resolve.map(response => response.json());
-      weather.value = await data[0];
-      
-      //filter the list index 40/8
-      const forecastOrigin = await data[1]
-      const forecastDays = filterByIncrement(forecastOrigin.list,  8, 40)
-
-      forecast.value = forecastDays
-    } catch(err) {
-      console.error(err);
+      return await fetch(url, opts);
     }
+    catch (err) {
+      errs.push(err);
+    }
+  } 
+  throw errs;
+};
+
+fetchWithRetry(urlWeather)
+  .then(response => response.json())
+  .then(data => weather.value = data)
+  .catch(err => console.error(err));
+
+
+async function weatherLocation(location) {
+  const urlWeather = `https://api.openweathermap.org/data/2.5/weather?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`;
+  const response = await fetch(urlWeather);
+  if (response.status === 404) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  const data = await response.json();
+  console.log(data);
+  weather.value = data
 }
 
-const filterByIncrement = (iterable, increment, end) => {
-  const iterableLength = iterable.length
-  const result = [];
-  let index = increment;
-  while(index < iterableLength && index < end) {
-    result.push(iterable[index])
-    index += increment;
-  }
-  return result;
-}
+weatherLocation().catch(error => {
+  console.log(error.message); // 'An error has occurred: 404'
+});
+
+// const weatherLocation = async (location) => {
+  
+  
+  
+  
+//   fetch(urlWeather)
+//   .then(response => 
+//     // if(response.status === 404) {
+//     //   throw 'City no found'
+//     // }
+//       response.json()
+    
+//   )
+//   .then(data => {
+//     console.log(data)
+//     weather.value = await data
+//   }).catch(error => console.error(error))
+  
+  
+// //   fetch(urlWeather)
+// //   .then((response) => {
+// //     if (response.status === 404) {
+// //       throw 'City no found'
+// //     }
+// //     if(response.ok) {
+// //       return response
+// //     }
+// //   // console.log('Looks like there was a problem. Status Code: ' + response.status);
+// //         // weather.value = response.data;
+
+// // //  return response.data
+  
+// //  }).then(res => console.log(res.json()) )
+
+//   // const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?${location}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
+//   //   try {
+    
+//   //     const resolve = await Promise.all([
+//   //       fetch(urlWeather),
+//   //       fetch(urlForecast)
+//   //     ])
+//   //     if (resolve[0].status == 404 || resolve[1].status == 404) {
+//   //       throw 'City no found'
+//   //     }
+//   //     const data = resolve.map(response => response.json());
+//   //     weather.value = await data[0];
+      
+//   //     //filter the list index 40/8
+//   //     const forecastOrigin = await data[1]
+//   //     const forecastDays = filterByIncrement(forecastOrigin.list,  8, 40)
+
+//   //     forecast.value = forecastDays
+//   //   } catch(err) {
+//   //     console.error(err);
+//   //   }
+
+// }
 
 function locationBySearch(query) {
   if (query != ''){
@@ -85,21 +143,19 @@ async function getLocation() {
       />
     </header>
     <main class="flex flex-grow md:justify-center">
-      <div class="flex flex-col sm:flex-row gap-4" v-if="weather && forecast.length > 0">
+      <div class="flex flex-col sm:flex-row gap-4" v-if="weather">
        <div class="w-full sm:w-6/12 md:w-6/12 grow">
-
           <CurrentWeather
-              :name="name"
-              :temp="weather.main.temp"
-              :feels_like="weather.main.feels_like"
-              :main=" weather.weather[0].main"
-              :description="weather.weather[0].description"
-              :sunrise="sunrise"
-              :sunset="sunset"
-              :humidity="weather.main.humidity"
-              :wind_speed="weather.wind.speed"
-              :isCelsius="isCelsius"
-              />
+            :name="weather.name"
+            :temp="weather.main.temp"
+            :feels_like="weather.main.feels_like"
+            :main=" weather.weather[0].main"
+            :description="weather.weather[0].description"
+            :sunrise="sunrise"
+            :sunset="sunset"
+            :humidity="weather.main.humidity"
+            :wind_speed="weather.wind.speed"
+            :isCelsius="isCelsius"/>
         </div>
       </div>
           <h2 v-else-if="stateNavigator" class="m-auto text-2xl font-bold text-sky-800 dark:text-sky-400">Geolocation not allowed</h2>
